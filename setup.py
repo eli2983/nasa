f0df97986dc3064071d2a514788218c37a6ec431
Eli Shemesh <eli.shemesh@algosec.com>
2018-11-11 14:44:09 +0200

    environment setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nasa',
+      version='0.1',
+      description='Nasa project',
+      url='http://github.com/eli2983/nasa',
+      author='Eli Shemesh',
+      author_email='eli2983@gmail.com',
+      license='MIT',
+      packages=['nasa'],
+      zip_safe=False)
